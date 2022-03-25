f2461bf80268424238038dd59359233931669d01
Ephie Oyedoh <ephieoyedoh@Ephies-Work-MacBook-Pro.local>
2022-03-25 11:25:21 +0000

    writting test for routes and controllers

--- journal_app/tests.py
+++ journal_app/tests.py
@@ -1,10 +1,5 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from .models import Posts, Quotes
-
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
 
 
 class PostsModelTest(TestCase):
