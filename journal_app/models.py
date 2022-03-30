3c1957a7a35dd98e594d9800fe2ab56e09343e44
Ephie Oyedoh <ephieoyedoh@Ephies-Work-MacBook-Pro.local>
2022-03-30 15:10:38 +0100

    fixing tests by switching timezone to datetime

--- journal_app/models.py
+++ journal_app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.utils import timezone
+import datetime
 
 
 class Posts(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(default='SOME STRING')
-    date = models.DateField(default=timezone.now)
+    date = models.DateField(default=datetime.date.today)
 
     def __str__(self):
         return self.title
@@ -16,7 +16,7 @@ class Posts(models.Model):
 
 class Quotes(models.Model):
     title = models.CharField(max_length=100)
-    date = models.DateField(default=timezone.now)
+    date = models.DateField(default=datetime.date.today)
     content = models.TextField(default='SOME QUOTE')
 
     def __str__(self):
