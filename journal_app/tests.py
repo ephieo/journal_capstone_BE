c1cfc7f4fac08be0a96110a15744ac81ac7c68b1
Ephie Oyedoh <ephieoyedoh@Ephies-Work-MacBook-Pro.local>
2022-03-30 15:03:24 +0100

    fixing failing ci

--- journal_app/tests.py
+++ journal_app/tests.py
@@ -6,11 +6,10 @@ class PostsModelTest(TestCase):
 
     def testPostExists(self):
         post = Posts.objects.create(
-            title='day at park', content='I went to the park', img_link="some link")
+            title='day at park', content='I went to the park')
 
         self.assertEqual(str(post.title), 'day at park')
         self.assertEqual(str(post.content), 'I went to the park')
-        self.assertEqual(str(post.img_link), 'some link')
 
     def test_verbose_name_plural(self):
         self.assertEqual(str(Posts._meta.verbose_name_plural), "Posts")
@@ -19,11 +18,10 @@ class PostsModelTest(TestCase):
 class QuotesModelTest(TestCase):
     def testQuoteExists(self):
         quote = Quotes.objects.create(
-            title='clueless', content='As If!', img_link="some link")
+            title='clueless', content='As If!')
 
         self.assertEqual(str(quote.title), 'clueless')
         self.assertEqual(str(quote.content), 'As If!')
-        self.assertEqual(str(quote.img_link), 'some link')
 
     def test_verbose_name_plural(self):
         self.assertEqual(str(Quotes._meta.verbose_name_plural), "Quotes")
@@ -46,10 +44,10 @@ class AllRoutesTest(TestCase):
 
     def testPostsPostRequest(self):
         response = self.client.post(
-            '/quotes/', {'title': 'clueless', 'content': 'As if!', 'img_link': 'an img of cher'})
+            '/quotes/', {'title': 'clueless', 'content': 'As if!'})
         self.assertEqual(response.status_code, 201)
 
     def testQuotesPostRequest(self):
         response = self.client.post(
-            '/quotes/', {'title': 'trip to athens', 'content': 'I saw the acropolis !!!', 'img_link': 'an img of the acropolis'})
+            '/quotes/', {'title': 'trip to athens', 'content': 'I saw the acropolis !!!'})
         self.assertEqual(response.status_code, 201)
